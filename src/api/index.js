2470c204e077ce98d4c09c48a11ef6c6338d17af
abhijithmk <abhijithj331@gmail.com>
2023-03-08 14:56:34 +0530

    first

---
+++ src/api/index.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8000/api";
+// const BASE_URL = "https://gstregistration-production.up.railway.app/api";
+
+const API = axios.create({ baseURL: BASE_URL });
+
+export const initializePayment = async (userData) => {
+	const response = await API.post("/payment/process-payment", userData);
+	return response;
+};
+
+export const confirmPayment = async (paymentDetails, userData) => {
+	console.log(userData, "api");
+	const response = await API.post("/payment/confirm-payment", [
+		paymentDetails,
+		userData
+	]);
+	// return response;
+	console.log(response)
+
+};
+
+export const adminLogin = async (loginData) => {
+	const response = await API.post("/admin", loginData);
+	console.log(response);
+	return response;
+};
+
+export const fetchData = async () => {
+	const response = await API.get("/admin/data");
+	return response;
+};
