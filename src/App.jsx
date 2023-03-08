474aece26e4630b491c2dbb4ae41c059ac3545fe
abhijithmk <abhijithj331@gmail.com>
2023-03-08 15:48:10 +0530

    four

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route
-					path="/admin/login"
+					path="/adminlogin"
 					element={
 						// <PublicRoute>
 						// 	<AdminLogin />
